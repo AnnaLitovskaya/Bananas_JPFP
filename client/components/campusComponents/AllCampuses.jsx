009dd4ab2eb0b5715842c8423fff754e3513d825
Anna Litovskaya <Litovskaya09@gmail.com>
2021-04-02 20:25:24 -0400

    campus sort complete

--- client/components/campusComponents/AllCampuses.jsx
+++ client/components/campusComponents/AllCampuses.jsx
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getCampuses } from '../../store/storeComponents/campusStoreComponents/getCampuses';
 import CampusTab from './CampusTab.jsx';
+import { orderBy } from 'lodash';
 
 class AllCampuses extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      campuses: props.campuses,
+    };
     this.addCampus = this.addCampus.bind(this);
+    this.onSelect = this.onSelect.bind(this);
   }
   componentDidMount() {
     this.props.getCampuses();
@@ -16,8 +21,26 @@ class AllCampuses extends Component {
     this.props.history.push('/campuses/addCampus');
   }
 
+  onSelect(ev) {
+    let campuses;
+    if (ev.target.value === 'numStudents') {
+      campuses = orderBy(
+        this.props.campuses,
+        function (campus) {
+          return campus.Students.length;
+        },
+        ['asc']
+      );
+    } else {
+      campuses = orderBy(this.props.campuses, [ev.target.value], ['asc']);
+    }
+    this.setState({ campuses });
+  }
+
   render() {
-    const campuses = this.props.campuses;
+    const campuses = this.state.campuses.length
+      ? this.state.campuses
+      : this.props.campuses;
     if (campuses.length === 0) {
       return (
         <div className="center">
@@ -32,6 +55,16 @@ class AllCampuses extends Component {
           <div className="listHeader">
             <h1>All Campuses</h1>
             <button onClick={this.addCampus}>Add Campus</button>
+          </div>
+          <div>
+            <label className="sort" htmlFor="campusSort">
+              Sort Campus By:{' '}
+            </label>
+            <select name="campusSort" onChange={this.onSelect}>
+              <option value={''}>{'--Sort Selection--'}</option>
+              <option value={'numStudents'}>{'# Of Students'}</option>
+              <option value={'name'}>{'Campus name'}</option>
+            </select>
           </div>
           <div id="campusListing">
             {campuses.map((campus) => {
